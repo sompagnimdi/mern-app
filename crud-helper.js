e8ba1592f087eec2bc6c17a584008677f92eed84
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2023-01-03 23:26:31 -0600

    making progress

---
+++ crud-helper.js
@@ -0,0 +1,15 @@
+//Connect to database
+require('dotenv').config();
+require('./config/database');
+
+// Require the Mongoose models
+const User = require('./models/User');
+// const Item = require('./models/item');
+// const Category = require('./models/category');
+// const Order = require('./models/order');
+const Item = require('./models/item');
+const Category = require('./models/category');
+const Order = require('./models/order');
+
+let user, item, category, order;
+let users, items, categories, orders;
